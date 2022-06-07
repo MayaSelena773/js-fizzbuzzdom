3dd3c160926315b23b3fb58a9dec709646a49f75
MayaSelena773 <maya.bono721@gmail.com>
2022-06-07 18:10:26 +0200

    fizzbuzznumbers

---
+++ milestone-1/js/script.js
@@ -0,0 +1,26 @@
+
+//Stampare i numeri da 1 a 100
+//Per i numeri multipli di 3 stampare Fizz
+//Per i numeri multipli di 5 stampare Buzz
+//Per i numeri che sono sia multipli di 3 che di 5 stampare FizzBuzz.
+//Stampare in console.
+
+let fizzBuzzNumber;
+
+for(let i = 0; i <= 100; i++){
+
+    if( i % 3 === 0 && i % 5 === 0) {
+        fizzBuzzNumber = 'FizzBuzz';
+
+    }else if( i % 3 === 0) {
+        fizzBuzzNumber = 'Fizz';
+
+    }else if( i % 5 === 0) {
+        fizzBuzzNumber = 'Buzz';
+
+    }else {
+        fizzBuzzNumber = i;
+    }
+    console.log(fizzBuzzNumber);
+}
+
